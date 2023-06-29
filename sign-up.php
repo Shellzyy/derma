940c70ff4c4367fe41899ca5530377b174441f64
Shellzy <andr.sheptitsk@gmail.com>
2023-06-29 20:15:24 +0300

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -39,8 +39,62 @@ autoCompileLess('./assets/less/style.less', './assets/css/style.css');
 </head>
 
 <body>
-    <div class="wrapper">
-        <section class="sec19">
+    <div class="wrapper signup-wrap">
+        <div id="mobile-menu">
+            <div class="block df">
+                <div class="logo">
+                    <img src="./assets/img/logo-footer.svg" alt="">
+                </div>
+                <form action="#">
+                    <input type="text" placeholder="Пошук">
+                    <button><img src="./assets/img/search-icon.svg" alt=""></button>
+                </form>
+                <div class="accordeon">
+                    <div class="accordeon__line">
+                        Vichy
+                    </div>
+                    <div class="accordeon__content">
+                        <a href="#">Про бренд</a>
+                        <a href="#">Каталог продуктів</a>
+                    </div>
+                    <div class="accordeon__line">
+                        Cerave
+                    </div>
+                    <div class="accordeon__content">
+                        <a href="#">Про бренд</a>
+                        <a href="#">Каталог продуктів</a>
+                    </div>
+                    <div class="accordeon__line">
+                        La Roche-Posay
+                    </div>
+                    <div class="accordeon__content">
+                        <a href="#">Про бренд</a>
+                        <a href="#">Каталог продуктів</a>
+                    </div>
+                </div>
+                <ul>
+                    <li><a href=""><span>Онлайн-навчання</span></a></li>
+                    <li><a href=""><span>Заходи</span></a></li>
+                    <li><a href=""><span>Рейтинг</span></a></li>
+                    <li><a href=""><span>Новини</span></a></li>
+                </ul>
+                <div class="profile">
+                    <a href="#">Профіль</a>
+                </div>
+                <div class="help-mobile">
+                    <a href="#">Технічна підтримка</a>
+                </div>
+            </div>
+        </div>
+        <div class="menu">
+            <!-- Кнопка Мобильного Меню -->
+            <button id="burger-menu">
+                <div class="box box_item1"></div>
+                <div class="box box_item2"></div>
+                <div class="box box_item3"></div>
+            </button>
+        </div>
+        <section class="sec20">
             <div class="container">
                 <div class="logo">
                     <img src="./assets/img/sign-logo.svg" alt="">
@@ -49,7 +103,7 @@ autoCompileLess('./assets/less/style.less', './assets/css/style.css');
                     <div class="step step-first active">
                         <div class="top-content">
                             <div class="logo-form"><img src="./assets/img/signform-logo.svg" alt=""></div>
-                            <span>Увійти до кабінету Derma Club</span>
+                            <span>Створення аккаунт Derma Club</span>
                         </div>
                         <div class="content">
                             <label>
@@ -60,61 +114,124 @@ autoCompileLess('./assets/less/style.less', './assets/css/style.css');
                                 пароль
                                 <input type="text">
                             </label>
-                            <div class="bottom-btn">
-                                <label class="container-check">Запамʼятати мене
-                                    <input type="checkbox" checked="checked">
-                                    <span class="checkmark"></span>
-                                </label>
-                                <a href="#">Не памʼятаю пароль</a>
-                            </div>
-                            <button>Увійти</button>
-                            <a href="#" class="signup">Створити аккаунт</a>
+                            <label>
+                                повторний пароль
+                                <input type="text">
+                            </label>
+                            <button>Далі</button>
+                            <a href="#">В мене вже є аккаунт</a>
                         </div>
                     </div>
                     <div class="step step-second">
-                        <div class="logo-form"><img src="./assets/img/signform-logo.svg" alt=""></div>
-                        <span>Двофакторна автентифікація</span>
+                        <div class="top-content">
+                            <div class="logo-form"><img src="./assets/img/signform-logo.svg" alt=""></div>
+                            <span>Створення аккаунт Derma Club</span>
+                        </div>
                         <div class="content">
-                            <p>На ваш телефон <span>+380 *** ** 02</span> надіслано повідомлення з кодом підтвердження.
-                                Введіть код, щоб продовжити.</p>
-                            <div class="vcode" id="vcode">
-                                <input type="phone" class="vcode-input" maxlength="1" id="vcode1">
-                                <input type="phone" class="vcode-input" maxlength="1">
-                                <input type="phone" class="vcode-input" maxlength="1">
-                                <input type="phone" class="vcode-input" maxlength="1">
-                                <input type="phone" class="vcode-input" maxlength="1">
-                            </div>
-                            <a href="#" class="resend">Надіслати код повторно</a>
+                            <label>
+                                ПІБ (Українською)
+                                <input type="text">
+                            </label>
+                            <label>
+                                номер телефона
+                                <input type="text">
+                            </label>
+                            <label>
+                                Дата народження
+                                <input type="text">
+                            </label>
                             <button>Далі</button>
-                            <a href="#" class="a-sign">В мене вже є аккаунт</a>
+                            <a href="#">В мене вже є аккаунт</a>
                         </div>
                     </div>
                     <div class="step step-third">
-                        <div class="logo-form"><img src="./assets/img/signform-logo.svg" alt=""></div>
-                        <span>Увійти до кабінету Derma Club</span>
-                        <div class="error">
-                            <p>Ваш обліковий запис в процесі активації. Наша команда займається вашим запитом
-                                і очікується, що ви отримаєте відповідь протягом трьох робочих днів.</p>
+                        <div class="top-content">
+                            <div class="logo-form"><img src="./assets/img/signform-logo.svg" alt=""></div>
+                            <span>Створення аккаунт Derma Club</span>
                         </div>
                         <div class="content">
-                            <label>
-                                Email
-                                <input type="text">
-                            </label>
-                            <label>
-                                пароль
-                                <input type="text">
-                            </label>
+                            <div class="choose-item">
+                                <div class="choose-photo">
+                                    <span>Додати фото</span>
+                                </div>
+                                <div class="delete-photo">Видалити фото</div>
+                            </div>
+                        </div>
+                        <div class="content">
+                            <p>Будь ласка, виберіть зображення у форматі jpeg, jpg, png або gif, розміром не більше
+                                <span>5 МБ.</span></p>
+                            <p><span>Ви можете пропустити цей крок.</span></p>
+                            <button>Далі</button>
+                            <a href="#">В мене вже є аккаунт</a>
+                        </div>
+                    </div>
+                    <div class="step step-fourth">
+                        <div class="top-content">
+                            <div class="logo-form"><img src="./assets/img/signform-logo.svg" alt=""></div>
+                            <span>Створення аккаунт Derma Club</span>
+                        </div>
+                        <div class="content">
+                            <div class="selected-item">
+                                <span>Ваша посада</span>
+                                <div class="custom-select">
+                                    <select>
+                                        <option value="0">0</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="selected-item">
+                                <span>Місто</span>
+                                <div class="custom-select">
+                                    <select>
+                                        <option value="0">0</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="selected-item">
+                                <span>Мережа, в якій працюєте</span>
+                                <div class="custom-select">
+                                    <select>
+                                        <option value="0">0</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="selected-item">
+                                <span>Адреса аптеки</span>
+                                <div class="custom-select">
+                                    <select>
+                                        <option value="0">0</option>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <p>Якщо Ви не знайшли адреси Вашої аптеки в списку, зверніться по допомогу до Технічної
+                                підтримки.</p>
                             <div class="bottom-btn">
-                                <label class="container-check">Запамʼятати мене
+                                <label class="container-check">Я ознайомився (-лася) з Політикою конфіденційності та даю
+                                    згоду на обробку моїх персональних даних
                                     <input type="checkbox" checked="checked">
                                     <span class="checkmark"></span>
                                 </label>
-                                <a href="#">Не памʼятаю пароль</a>
                             </div>
-                            <button>Увійти</button>
-                            <a href="#" class="signup">Створити аккаунт</a>
+                            <button>Далі</button>
+                            <a href="#">В мене вже є аккаунт</a>
                         </div>
+                    </div>
+                    <div class="step step-fifth">
+                        <div class="logo-form"><img src="./assets/img/signup-il.svg" alt=""></div>
+                        <span>Ласкаво просимо на навчальну платформу Derma Club!</span>
+                        <p>Ваш акаунт буде активований протягом трьох робочих днів нашим торговим представником.</p>
                     </div>
                 </form>
                 <div class="partners">
@@ -136,16 +253,61 @@ autoCompileLess('./assets/less/style.less', './assets/css/style.css');
     <link rel="stylesheet" href="./assets/css/swiper/swiper-bundle.min.css">
     <script src="./assets/js/swiper/swiper-bundle.min.js"></script>
     <script>
-        $('.sec19 .container form .step-first .content button').click(function () {
+        $('.sec20 .container form .step-first .content button').click(function () {
             $('.step').removeClass('active');
             $('.step-second').addClass('active');
         });
     </script>
     <script>
-        $('.sec19 .container form .step-second .content button').click(function () {
+        $('.sec20 .container form .step-second .content button').click(function () {
             $('.step').removeClass('active');
             $('.step-third').addClass('active');
         });
+    </script>
+    <script>
+        $('.sec20 .container form .step-third .content button').click(function () {
+            $('.step').removeClass('active');
+            $('.step-fourth').addClass('active');
+        });
+    </script>
+    <script>
+        $('.sec20 .container form .step-fourth .content button').click(function () {
+            $('.step').removeClass('active');
+            $('.step-fifth').addClass('active');
+        });
+    </script>
+    <script>
+        function choosePhoto(target) {
+            // create new file input
+            let input = document.createElement("input");
+            input.setAttribute("type", "file");
+            // listen to change event
+            input.addEventListener("change", e => {
+                showPreview(e.target.files[0], target);
+            });
+            // click it programmatically
+            input.click();
+        }
+
+        function showPreview(file, target) {
+            // check file type
+            if (!file || !file.type || file.type.indexOf("image") !== 0) {
+                return;
+            }
+            // load photo preview
+            var reader = new FileReader();
+            reader.onload = e => {
+                // show loaded preview
+                target.style.backgroundImage = `url("${e.target.result}")`;
+                // add class .preview
+                target.classList.add("preview");
+                $('.delete-photo').addClass('active');
+            };
+            reader.readAsDataURL(file);
+        }
+
+        let div = document.querySelector(".choose-photo");
+        div.addEventListener("click", () => choosePhoto(div));
     </script>
     <script>
         var vcode = (function () {
