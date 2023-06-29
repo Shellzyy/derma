d512133900d1271ab3dcf0289b39609af16bc362
Shellzy <andr.sheptitsk@gmail.com>
2023-06-30 01:42:02 +0300

    Create result-state2.php

---
+++ result-state2.php
@@ -0,0 +1,42 @@
+<?php require "header.php" ?>
+
+<div class="breadcrumbs">
+    <div class="container">
+        <a href="#">Derma Club</a>
+        <a href="#">Онлайн-навчання</a>
+        <a href="#">Тестування «Сонцезахисна гама Антеліос від бренду La Roche-Posay»</a>
+    </div>
+</div>
+<section class="sec27">
+    <div class="container">
+        <div class="title">
+            <h2>Тестування «Сонцезахисна гама Антеліос від бренду La Roche-Posay»</h2>
+        </div>
+        <div class="content">
+            <div class="photo">
+                <img src="./assets/img/result-il2.svg" alt="">
+            </div>
+            <div class="result">
+                <p>На жаль, тест не пройдено. Ви добре відповіли на <span>2</span> із 10 запитань і не набрали потрібної
+                    кількості правильних відповідей.</p>
+            </div>
+            <div class="btns">
+                <a href="#">Пройти тест ще раз</a>
+                <a href="#">Переглянути модуль</a>
+            </div>
+            <a href="#" class="show-answer">Дивитись відповіді</a>
+        </div>
+    </div>
+</section>
+<style>
+    body {
+        background: #F4F4F8;
+    }
+
+    footer {
+        background: #F4F4F8;
+    }
+</style>
+
+<?php require "modal.php" ?>
+<?php require "footer.php" ?>
