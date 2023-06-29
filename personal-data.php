3a7d46134dc8634631f683ccbdc92326abbc07b0
Shellzy <andr.sheptitsk@gmail.com>
2023-06-30 01:42:05 +0300

    Create personal-data.php

---
+++ personal-data.php
@@ -0,0 +1,185 @@
+<?php require "header.php" ?>
+<section class="sec24">
+    <div class="container">
+        <div class="item-nav">
+            <ul>
+                <li>
+                    <a href="#">
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M16.5 8.58336C16.5029 9.68325 16.2459 10.7683 15.75 11.75C15.162 12.9265 14.2581 13.916 13.1395 14.6078C12.021 15.2995 10.7319 15.6662 9.41667 15.6667C8.31678 15.6696 7.23176 15.4126 6.25 14.9167L1.5 16.5L3.08333 11.75C2.58744 10.7683 2.33047 9.68325 2.33333 8.58336C2.33384 7.26815 2.70051 5.97907 3.39227 4.86048C4.08402 3.7419 5.07355 2.838 6.25 2.25002C7.23176 1.75413 8.31678 1.49716 9.41667 1.50002H9.83333C11.5703 1.59585 13.2109 2.32899 14.441 3.55907C15.671 4.78915 16.4042 6.42973 16.5 8.16669V8.58336Z"
+                                stroke="#071027" stroke-width="1.66667" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
+                        <span>Навчання</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M18.3332 9.2333V9.99997C18.3321 11.797 17.7503 13.5455 16.6743 14.9848C15.5983 16.4241 14.0859 17.477 12.3626 17.9866C10.6394 18.4961 8.79755 18.4349 7.1119 17.8121C5.42624 17.1894 3.98705 16.0384 3.00897 14.5309C2.03089 13.0233 1.56633 11.24 1.68457 9.4469C1.80281 7.65377 2.49751 5.94691 3.66507 4.58086C4.83263 3.21482 6.41049 2.26279 8.16333 1.86676C9.91617 1.47073 11.7501 1.65192 13.3915 2.3833M18.3332 3.3333L9.99984 11.675L7.49984 9.17497"
+                                stroke="#071027" stroke-width="1.66667" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
+                        <span>Історія</span>
+                    </a>
+                </li>
+                <li class="active">
+                    <a href="#">
+                        <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M14.6668 16.5V14.8333C14.6668 13.9493 14.3156 13.1014 13.6905 12.4763C13.0654 11.8512 12.2176 11.5 11.3335 11.5H4.66683C3.78277 11.5 2.93493 11.8512 2.30981 12.4763C1.68469 13.1014 1.3335 13.9493 1.3335 14.8333V16.5M11.3335 4.83333C11.3335 6.67428 9.84111 8.16667 8.00016 8.16667C6.15921 8.16667 4.66683 6.67428 4.66683 4.83333C4.66683 2.99238 6.15921 1.5 8.00016 1.5C9.84111 1.5 11.3335 2.99238 11.3335 4.83333Z"
+                                stroke="#071027" stroke-width="1.66667" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
+                        <span>Особисті дані</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M1.5 14V9C1.5 7.01088 2.29018 5.10322 3.6967 3.6967C5.10322 2.29018 7.01088 1.5 9 1.5C10.9891 1.5 12.8968 2.29018 14.3033 3.6967C15.7098 5.10322 16.5 7.01088 16.5 9V14M16.5 14.8333C16.5 15.2754 16.3244 15.6993 16.0118 16.0118C15.6993 16.3244 15.2754 16.5 14.8333 16.5H14C13.558 16.5 13.134 16.3244 12.8215 16.0118C12.5089 15.6993 12.3333 15.2754 12.3333 14.8333V12.3333C12.3333 11.8913 12.5089 11.4674 12.8215 11.1548C13.134 10.8423 13.558 10.6667 14 10.6667H16.5V14.8333ZM1.5 14.8333C1.5 15.2754 1.67559 15.6993 1.98816 16.0118C2.30072 16.3244 2.72464 16.5 3.16667 16.5H4C4.44203 16.5 4.86595 16.3244 5.17851 16.0118C5.49107 15.6993 5.66667 15.2754 5.66667 14.8333V12.3333C5.66667 11.8913 5.49107 11.4674 5.17851 11.1548C4.86595 10.8423 4.44203 10.6667 4 10.6667H1.5V14.8333Z"
+                                stroke="#071027" stroke-width="1.66667" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
+                        <span>Зворотній зв'язок</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M6.5 16.5H3.16667C2.72464 16.5 2.30072 16.3244 1.98816 16.0118C1.67559 15.6993 1.5 15.2754 1.5 14.8333V3.16667C1.5 2.72464 1.67559 2.30072 1.98816 1.98816C2.30072 1.67559 2.72464 1.5 3.16667 1.5H6.5M12.3333 13.1667L16.5 9M16.5 9L12.3333 4.83333M16.5 9H6.5"
+                                stroke="#071027" stroke-width="1.66667" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
+                        <span>Вихід</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="item-personal">
+            <div class="s1">
+                <div class="title">
+                    <span>Особисті дані</span>
+                </div>
+                <form action="">
+                    <div class="personal-data">
+                        <div class="item">
+                            <span>імʼя та прізвище</span>
+                            <input type="text">
+                        </div>
+                        <div class="item">
+                            <span>телефон</span>
+                            <input type="text">
+                        </div>
+                        <div class="item">
+                            <span>електронна пошта</span>
+                            <input type="text">
+                        </div>
+                        <div class="item">
+                            <span>дата народження</span>
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div class="personal-location">
+                        <div class="item">
+                            <span>Місто</span>
+                                <div class="custom-select">
+                                    <select>
+                                        <option value="0"></option>
+                                        <option value="1">Audi</option>
+                                        <option value="2">BMW</option>
+                                        <option value="3">Citroen</option>
+                                    </select>
+                                </div>
+                        </div>
+                        <div class="item">
+                            <span>меража</span>
+                                <div class="custom-select">
+                                    <select>
+                                        <option value="0"></option>
+                                        <option value="1">Audi</option>
+                                        <option value="2">BMW</option>
+                                        <option value="3">Citroen</option>
+                                    </select>
+                                </div>
+                        </div>
+                        <div class="item">
+                            <span>Адреса аптеки</span>
+                                <div class="custom-select">
+                                    <select>
+                                        <option value="0"></option>
+                                        <option value="1">Audi</option>
+                                        <option value="2">BMW</option>
+                                        <option value="3">Citroen</option>
+                                    </select>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="personal-social">
+                        <div class="item">
+                            <span>Instagram</span>
+                            <input type="text">
+                        </div>
+                        <div class="item">
+                            <span>youtube</span>
+                            <input type="text">
+                        </div>
+                        <div class="item">
+                            <span>tiktok</span>
+                            <input type="text">
+                        </div>
+                    </div>
+                    <div class="but">
+                        <button>Зберегти зміни</button>
+                    </div>
+                </form>
+            </div>
+            <div class="s2">
+                <div class="title">
+                    <span>Торговий представник</span>
+                </div>
+                <div class="name-phone">
+                    <div class="item">
+                        <span>імʼя</span>
+                        <p>Онофрієнко Дмитро Ігорович</p>
+                    </div>
+                    <div class="item">
+                        <span>Номер телефона</span>
+                        <p>+38 050 070 26 31</p>
+                    </div>
+                </div>
+            </div>
+            <div class="s3">
+                <div class="title">
+                    <span>Безпека</span>
+                </div>
+                <div class="settings">
+                    <div class="password">
+                        <div class="text">
+                            <span>Пароль</span>
+                            <p>Встановіть новий пароль для входу в обліковий запис.</p>
+                        </div>
+                        <div class="button">
+                            <a href="#">Змінити</a>
+                        </div>
+                    </div>
+                    <div class="account_del">
+                        <div class="text">
+                            <span>видалити аккаунт</span>
+                            <p>Ви більше не зможете користуватись акаунтом. Всі бали будуть втрачені.</p>
+                        </div>
+                        <div class="button">
+                            <a href="#">Видалити</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php require "modal.php" ?>
+<?php require "footer.php" ?>
