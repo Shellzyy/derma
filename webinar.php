9cd367d84c1bad962788cebd906aed1ac29a94fe
Shellzy <andr.sheptitsk@gmail.com>
2023-06-30 01:42:00 +0300

    Create webinar.php

---
+++ webinar.php
@@ -0,0 +1,108 @@
+<?php require "header.php" ?>
+<section class="sec28">
+    <div class="container">
+        <div class="main-content">
+            <div data-da=".courses-adapt,992,2" class="courses">
+                <div class="accordeon">
+                    <div class="accordeon__line">
+                        <span class="module">Вебінари</span>
+                        <div class="stats">
+                            <span class="module">Вебінари</span>
+                            <div class="balls">0/80 Балів</div>
+                        </div>
+                    </div>
+                    <div class="accordeon__content">
+                        <div class="content-line">
+                            <div class="line-title">
+                                <div class="icon">
+                                    <img src="./assets/img/line-icon1.svg" alt="">
+                                </div>
+                                <div class="line-txt">
+                                    <div class="balls-mobile active">8 Балів</div>
+                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
+                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
+                                </div>
+                            </div>
+                            <div class="stats">
+                            <div class="line-date">07 березня – 18:00</div>
+                                <div class="balls active">8 Балів</div>
+                                <div class="btn active"><a href="#">Розпочати</a></div>
+                            </div>
+                        </div>
+                        <div class="content-line">
+                            <div class="line-title">
+                                <div class="icon">
+                                    <img src="./assets/img/line-icon1.svg" alt="">
+                                </div>
+                                <div class="line-txt">
+                                    <div class="balls-mobile active">8 Балів</div>
+                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
+                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
+                                </div>
+                            </div>
+                            <div class="stats">
+                            <div class="line-date">07 березня – 18:00</div>
+                                <div class="balls active">8 Балів</div>
+                                <div class="btn active"><a href="#">Розпочати</a></div>
+                            </div>
+                        </div>
+                        <div class="content-line">
+                            <div class="line-title">
+                                <div class="icon">
+                                    <img src="./assets/img/line-icon1.svg" alt="">
+                                </div>
+                                <div class="line-txt">
+                                    <div class="balls-mobile active">8 Балів</div>
+                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
+                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
+                                </div>
+                            </div>
+                            <div class="stats">
+                            <div class="line-date">07 березня – 18:00</div>
+                                <div class="balls active">8 Балів</div>
+                                <div class="btn active"><a href="#">Розпочати</a></div>
+                            </div>
+                        </div>
+                        <div class="content-line">
+                            <div class="line-title">
+                                <div class="icon">
+                                    <img src="./assets/img/line-icon1.svg" alt="">
+                                </div>
+                                <div class="line-txt">
+                                    <div class="balls-mobile active">8 Балів</div>
+                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
+                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
+                                </div>
+                            </div>
+                            <div class="stats">
+                            <div class="line-date">07 березня – 18:00</div>
+                                <div class="balls active">8 Балів</div>
+                                <div class="btn active"><a href="#">Розпочати</a></div>
+                            </div>
+                        </div>
+                        <div class="content-line">
+                            <div class="line-title">
+                                <div class="icon">
+                                    <img src="./assets/img/line-icon1.svg" alt="">
+                                </div>
+                                <div class="line-txt">
+                                    <div class="balls-mobile active">8 Балів</div>
+                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
+                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
+                                </div>
+                            </div>
+                            <div class="stats">
+                            <div class="line-date"></div>
+                                <div class="balls">0/10 Балів</div>
+                                <div class="btn active"><a href="#">Розпочати</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php require "modal.php" ?>
+<?php require "footer.php" ?>
