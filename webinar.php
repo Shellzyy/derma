e7d6c3f8cac8aa6a4dc795fce74411f16973935c
Shellzy <andr.sheptitsk@gmail.com>
2023-06-30 09:00:57 +0300

    Update webinar.php

--- webinar.php
+++ webinar.php
@@ -2,7 +2,7 @@
 <section class="sec28">
     <div class="container">
         <div class="main-content">
-            <div data-da=".courses-adapt,992,2" class="courses">
+            <div class="courses">
                 <div class="accordeon">
                     <div class="accordeon__line">
                         <span class="module">Вебінари</span>
@@ -15,86 +15,99 @@
                         <div class="content-line">
                             <div class="line-title">
                                 <div class="icon">
-                                    <img src="./assets/img/line-icon1.svg" alt="">
+                                    <img src="./assets/img/webinar-icon1.svg" alt="">
                                 </div>
                                 <div class="line-txt">
                                     <div class="balls-mobile active">8 Балів</div>
                                     <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
-                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
+                                    <div class="btn-mobile">
+                                        <a href="#">Дивитись</a>
+                                        <a href="#" class="done-btn">Пройдено</a>
+                                    </div>
                                 </div>
                             </div>
                             <div class="stats">
-                            <div class="line-date">07 березня – 18:00</div>
+                                <div class="line-date"></div>
                                 <div class="balls active">8 Балів</div>
-                                <div class="btn active"><a href="#">Розпочати</a></div>
+                                <div class="btn">
+                                    <a href="#">Дивитись</a>
+                                    <a href="#" class="done-btn">Пройдено</a>
+                                </div>
                             </div>
                         </div>
                         <div class="content-line">
                             <div class="line-title">
                                 <div class="icon">
-                                    <img src="./assets/img/line-icon1.svg" alt="">
+                                    <img src="./assets/img/webinar-icon2.svg" alt="">
                                 </div>
                                 <div class="line-txt">
-                                    <div class="balls-mobile active">8 Балів</div>
+                                    <div class="balls-mobile">0/10 Балів</div>
                                     <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
-                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
+                                    <div class="btn-mobile">
+                                        <a href="#">Дивитись</a>
+                                        <a href="#" class="done-btn">Пройти тест</a>
+                                    </div>
                                 </div>
                             </div>
                             <div class="stats">
-                            <div class="line-date">07 березня – 18:00</div>
-                                <div class="balls active">8 Балів</div>
-                                <div class="btn active"><a href="#">Розпочати</a></div>
-                            </div>
-                        </div>
-                        <div class="content-line">
-                            <div class="line-title">
-                                <div class="icon">
-                                    <img src="./assets/img/line-icon1.svg" alt="">
-                                </div>
-                                <div class="line-txt">
-                                    <div class="balls-mobile active">8 Балів</div>
-                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
-                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
-                                </div>
-                            </div>
-                            <div class="stats">
-                            <div class="line-date">07 березня – 18:00</div>
-                                <div class="balls active">8 Балів</div>
-                                <div class="btn active"><a href="#">Розпочати</a></div>
-                            </div>
-                        </div>
-                        <div class="content-line">
-                            <div class="line-title">
-                                <div class="icon">
-                                    <img src="./assets/img/line-icon1.svg" alt="">
-                                </div>
-                                <div class="line-txt">
-                                    <div class="balls-mobile active">8 Балів</div>
-                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
-                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
-                                </div>
-                            </div>
-                            <div class="stats">
-                            <div class="line-date">07 березня – 18:00</div>
-                                <div class="balls active">8 Балів</div>
-                                <div class="btn active"><a href="#">Розпочати</a></div>
-                            </div>
-                        </div>
-                        <div class="content-line">
-                            <div class="line-title">
-                                <div class="icon">
-                                    <img src="./assets/img/line-icon1.svg" alt="">
-                                </div>
-                                <div class="line-txt">
-                                    <div class="balls-mobile active">8 Балів</div>
-                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
-                                    <div class="btn-mobile active"><a href="#">Розпочати</a></div>
-                                </div>
-                            </div>
-                            <div class="stats">
-                            <div class="line-date"></div>
+                                <div class="line-date"></div>
                                 <div class="balls">0/10 Балів</div>
-                                <div class="btn active"><a href="#">Розпочати</a></div>
+                                <div class="btn">
+                                    <a href="#">Дивитись</a>
+                                    <a href="#" class="done-btn">Пройти тест</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content-line">
+                            <div class="line-title">
+                                <div class="icon">
+                                    <img src="./assets/img/webinar-icon2.svg" alt="">
+                                </div>
+                                <div class="line-txt">
+                                    <div class="balls-mobile">0/10 Балів</div>
+                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
+                                    <div class="btn-mobile"><a href="#">Дивитись</a></div>
+                                </div>
+                            </div>
+                            <div class="stats">
+                                <div class="line-date"></div>
+                                <div class="balls">0/10 Балів</div>
+                                <div class="btn"><a href="#">Дивитись</a></div>
+                            </div>
+                        </div>
+                        <div class="content-line">
+                            <div class="line-title">
+                                <div class="icon">
+                                    <img src="./assets/img/webinar-icon2.svg" alt="">
+                                </div>
+                                <div class="line-txt">
+                                    <div class="balls-mobile">0/10 Балів</div>
+                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
+                                    <div class="line-date">07 березня – 18:00</div>
+                                    <div class="btn-mobile"><a href="#">Приєднатись</a></div>
+                                </div>
+                            </div>
+                            <div class="stats">
+                                <div class="line-date">07 березня – 18:00</div>
+                                <div class="balls">0/10 Балів</div>
+                                <div class="btn"><a href="#">Приєднатись</a></div>
+                            </div>
+                        </div>
+                        <div class="content-line">
+                            <div class="line-title">
+                                <div class="icon">
+                                    <img src="./assets/img/webinar-icon3.svg" alt="">
+                                </div>
+                                <div class="line-txt">
+                                    <div class="balls-mobile active">8 Балів</div>
+                                    <span>Інтерактивний тренінг "Гама Антеліос. Засоби для обличчя".</span>
+                                    <div class="btn-mobile"><a href="#">Зареєструватись</a></div>
+                                </div>
+                            </div>
+                            <div class="stats">
+                                <div class="line-date"></div>
+                                <div class="balls">0/10 Балів</div>
+                                <div class="btn"><a href="#">Зареєструватись</a></div>
                             </div>
                         </div>
                     </div>
